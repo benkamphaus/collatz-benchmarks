c215bf648acae477c55989e4df03973cb2702373
Ben Kamphaus <ben.kamphaus@gmail.com>
2015-09-05 12:09:01 -0600

    auto-import, change timeit number behind previous failure to complete

--- test/benchmark.py
+++ test/benchmark.py
@@ -1,6 +1,9 @@
 from timeit import timeit
+import sys
 
 # invoke w/either python or pypy
+
+sys.path.append('src/python')
 
 if __name__ == "__main__":
     imports = "from collatz import collatz, collatz_repeat, collatz_reverse"
@@ -9,4 +12,4 @@ if __name__ == "__main__":
 # these don't finish at moment, Julia hits it in 0.83 and 1.1 seconds in what
 # should be same general complexity (but typed arrays, etc.)
 #    print(timeit("collatz_repeat(10000)", setup=imports))
-#    print(timeit("collatz_reverse(10000)", setup=imports))
+    print(timeit("collatz_reverse(10000)", setup=imports, number=5))
